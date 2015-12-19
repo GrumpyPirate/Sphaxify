454b410f3d6638eda17f5b022069bc53e9eb67ed
Stephanie Cobbold <steph.cobbold@gmail.com>
2015-12-19 02:14:15 +0000

    Loop thorugh resizeLevels to asynchronously add to merged stream

--- Gulpfile.js
+++ Gulpfile.js
@@ -55,33 +55,39 @@ var gulp = require('gulp'),
  * Task - optimise
  * -------------------------------------------------------------------------- */
 gulp.task('optimise', function() {
-		var pctScale = (size / initialSize * 100).toString() + '%',
-			customDirname = size.toString() + suff,
+	var initialSize = 512,
+		resizeLevels = 5, // Downsize the original pack this many times
+		suff = 'x', // suffix to add onto created size pack directories
+		fileStream = gulp.src(paths.img.src + '**', // source everything
+			{ base: paths.img.src }
+		),
+		mergedStream = new $.mergeStream();
+
+	function resizeStream (size) {
+		var pctScale = size.toString() + '%',
+			relativeSize = initialSize * size / 100,
+			customDirname = path.join(relativeSize + suff),
 			zipName = '[' + customDirname + '] Sphax Patch - ' + patchName + '.zip',
-			ignoreStuff = $.ignore('**/*.{psb,psd,DS_Store}'),
+			ignoreStuff = $.ignore('**/*.{psb,psd,DS_Store,db}'),
+			// Optimise these files:
 			filterPNG = $.filter(['**/*.png'], { restore: true }),
+			// Resize these files:
 			filterResizeables = $.filter([
 				'**/*.png',
+				'!**/{gui,guis,font,fonts}/**/*.png',
 				'!**/pack.png'
 			], { restore: true }),
-			// If there are images that need their transparency preserved, define them here, e.g.:
-			// filterThresholdable = $.filter(['**/*.png', '!**/items/icons/**/*.png'], { restore: true });
+			// Apply threshold filter to these files:
 			filterThresholdable = $.filter([
 				'**/*.png'
 			], { restore: true });
 
-		return stream
-			.pipe(ignoreStuff)
+		return fileStream
 			.pipe($.clone())
-			.pipe($.rename(function (path) {
-				// { dirname: 'Example Project 2',
-				//   basename: 'test1',
-				//   extname: '.png' }
-				path.dirname = customDirname + '/' + path.dirname;
-			}))
+			.pipe(ignoreStuff)
 			.pipe($.newer(paths.img.dest))
 			// Cache everything past this point, name the cache using dirname
-			.pipe($.cached(customDirname))
+			.pipe($.cached(relativeSize.toString()))
 			// Do the following steps to PNGs only( i.e. no .txt, .mcmeta files)
 			.pipe(filterPNG)
 				// Do the following to ONLY resizeables
@@ -110,11 +116,30 @@ gulp.task('optimise', function() {
 				.pipe($.imagemin(settings.imagemin))
 			// Restore non-PNG files to stream
 			.pipe(filterPNG.restore)
+			.pipe($.rename(function (thisPath) {
+				// { dirname: 'Example Project 2',
+				//   basename: 'test1',
+				//   extname: '.png' }
+				console.log(thisPath.dirname);
+				thisPath.dirname = customDirname + '/' + thisPath.dirname;
+			}))
 			.pipe(gulp.dest(paths.img.dest))
 			// zip everything up
 			.pipe($.zip(zipName))
 			.pipe(gulp.dest(paths.img.dest));
-	});
+	} // /function resizeStream
+
+
+	for (var i = 0; i < resizeLevels; i++) {
+		var size = 100 / Math.pow(2, i);
+
+		mergedStream.add(
+			resizeStream(size)
+		);
+	}
+
+	return mergedStream;
+});
 
 // default task
 // -----------------------------------------------------------------------------
